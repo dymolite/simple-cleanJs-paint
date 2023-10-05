be5d7d0600fe38f9ec1c0368309589823fe7ebf3
dymolite <78ah5v7@code.edu.az>
2023-10-06 01:18:53 +0400

    new features added and work area bug

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,39 @@
 const canvas = document.querySelector('.canvas');
-const brushSize = 12;
-const brushRadius = 100;
-const brushColor = 'red';
-const workAreaStart =window.innerWidth- canvas.clientWidth;
+const thicknessBtn = document.querySelectorAll(".thickness .thicknessBtn")
+const chooseColor = document.getElementById('choose-color');
+let brushSize = '10px';
+let brushRadius = 100;
+let brushColor = 'black';
+const workAreaStart =window.innerWidth - canvas.clientWidth;
+const workAreaStartEnd = window.innerWidth;
 
+chooseColor.addEventListener("change", () => {
+    brushColor = chooseColor.value;
+})
 
+thicknessBtn.forEach(thickBtn => {
+    thickBtn.addEventListener('click',(e) => {
+        const thicknessValue = thickBtn.nextElementSibling.innerHTML;
+        brushSize = thicknessValue;
+        console.log(brushSize);
+    })
+})
 window.addEventListener("mousemove", (event) => {
     if (event.buttons !== 1) return;
 
     if (workAreaStart <= event.screenX) paint(event);
+    console.log(workAreaStart , event.screenX);
 });
-
 
 function paint(event) {
     const brush = document.createElement("p");
+    
     brush.style.borderRadius = `${brushRadius}px`;
-    brush.style.width=`${brushSize}px`;
-    brush.style.height=`${brushSize}px`;
+    brush.style.width = brushSize;
+    brush.style.height= brushSize;
     brush.style.position = "absolute";
     brush.style.backgroundColor = brushColor;
-    brush.style.top = `${event.screenY - brushRadius +brushSize}px`;
+    brush.style.top = `${event.screenY - brushRadius + parseInt(brushSize)}px`;
     brush.style.left = `${event.screenX}px`;
     canvas.appendChild(brush);
 }
